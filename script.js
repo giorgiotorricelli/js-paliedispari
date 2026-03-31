b7d3a5ac41df1ada1f72c786910e0495cdaa8592
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-03-31 16:19:33 +0200

    completato l'sesercizio sui palindromi

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //FUNCTIONS
 
+function isPalindromo(parola) {
+    let primaMeta = parola.slice(0, Math.floor(parola.length / 2));
+    let secondaMeta = parola.slice(Math.ceil(parola.length / 2));
+    let secondaMetaInvertita = "";
+
+    for (let i = secondaMeta.length - 1; i >= 0; i--) {
+        secondaMetaInvertita += secondaMeta[i];
+    }
+    
+    
+    if (primaMeta === secondaMetaInvertita) {
+        console.log(`"${parola}" è un palindromo`);
+    } else {
+        console.log(`"${parola}" non è un palindromo`);
+    }
+}
+
+
+let parolaDaVerificare = prompt("Inserisci una parola o un numero:");
+isPalindromo(parolaDaVerificare);
+
